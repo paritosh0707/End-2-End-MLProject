d73382384cdd96132f33b8496b94a6acc8e72462
paritosh0707 <paritoshsharma0707@gmail.com>
2023-05-23 10:43:26 +0530

    implemented hyper parameter tuning

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -3,17 +3,18 @@ import sys
 import pandas as pd
 from src.exception import CustomException
 from src.logger import logging
-
+from data_transformation import DataTransformation
+from model_trainer import ModelTrainer
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
 @dataclass
 class dataIngestionConfig:
-    train_data_path:str = os.path.join('artifact','train.csv')
-    test_data_path:str = os.path.join('artifact','test.csv')
-    raw_data_path:str = os.path.join('artifact','data.csv')
+    train_data_path:str = os.path.join('artifacts','train.csv')
+    test_data_path:str = os.path.join('artifacts','test.csv')
+    raw_data_path:str = os.path.join('artifacts','data.csv')
     
-class dataIngestion:
+class DataIngestion:
     def __init__(self):
         self.ingestion_config = dataIngestionConfig()
 
@@ -29,13 +30,17 @@ class dataIngestion:
             train_set,test_set = train_test_split(df,test_size=0.2,random_state=42)
             train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
             test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
-            return(
-                self.ingestion_config.train_data_path,
-                self.ingestion_config.test_data_path
-            )
+            return self.ingestion_config.train_data_path,self.ingestion_config.test_data_path
+            
         except Exception as e:
             CustomException(e,sys)
 
 if __name__ =='__main__':
-    obj= dataIngestion()
-    obj.initiate_data_ingestion()
+    obj= DataIngestion()
+    train_data,test_data= obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_ = data_transformation.initialte_data_transformation(train_data,test_data)
+
+    model_trainer = ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_arr,test_arr))
